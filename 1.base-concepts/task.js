2aed9531366c16a7c015a81bc8c79c3bed9d5e76
Perova <av_perova@mail.ru>
2023-10-05 12:06:05 +0700

    Домашнее задание готово

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,31 @@
 "use strict"
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+	let arr = [];
+	let d = b ** 2 - 4 * a * c;
+
+	if (d < 0) {
+		return arr;
+	} else if (d === 0) {
+		let root = -b / (2 * a);
+		arr.push(root);
+		return arr;
+	} else if (d > 0) {
+		let root1 = (-b + Math.sqrt(d)) / (2 * a);
+		let root2 = (-b - Math.sqrt(d)) / (2 * a);
+		arr.push(root1, root2);
+		return arr;
+	}
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+	let payment;
+	let percentPerMonth;
+	let body;
+	let sum;
+	body = amount - contribution;
+	percentPerMonth = percent / 100 / 12;
+	payment = body * (percentPerMonth + (percentPerMonth / (((1 + percentPerMonth) ** countMonths) - 1)));
+	sum = payment * countMonths;
+	return Math.round(sum * 100) / 100;
 }
